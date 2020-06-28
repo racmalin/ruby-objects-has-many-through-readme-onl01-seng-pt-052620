ff22694615bd53bf898936277c87f3f157790511
Kevin C Malin <racmalin@yahoo.com>
2020-06-28 02:12:04 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -9,7 +9,12 @@ class Waiter
     @yrs_experience = yrs_experience
     @@all << self
   end
- 
+ def meals
+  Meal.all.select do |meal|
+    meal.customer == self
+  end
+end
+
   def self.all
     @@all
   end
